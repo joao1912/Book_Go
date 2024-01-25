662a41d01de63432f9b6668efa651cff74a23015
joão <joaopedrot1912@gmail.com>
2024-01-25 02:06:09 -0300

    coment

--- src/interface/routes/user/userRoutes.ts
+++ src/interface/routes/user/userRoutes.ts
@@ -1,9 +1,14 @@
+import { HttpRequest, HttpResponse } from "../../../adapters/HTTPAdapter/protocol";
 import Router from "../../../adapters/routeAdapter/protocol";
 
 const userRouter = new Router()
 
-userRouter.get('/', function() {
-    console.log('rota de teste')
+userRouter.get('/', function(req: HttpRequest, res: HttpResponse) {
+    
+    res.status(200).json('só pra testar')
+
 })
+
+//tem que testar com middlewares(e como tratar esse middlewares, precisam ser tipados em um contrato o next por exemplo)
 
 export default userRouter.getRoute()
