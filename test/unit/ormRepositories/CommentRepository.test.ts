9a536f156ce8e238c95b4b69083a2ef40d2ef3e6
joão <joaopedrot1912@gmail.com>
2024-02-02 14:15:36 -0300

    testes do commentRespoitory pronto

--- test/unit/ormRepositories/CommentRepository.test.ts
+++ test/unit/ormRepositories/CommentRepository.test.ts
@@ -1,8 +1,8 @@
 import { addBook } from "../../../src/adapters/ormAdapter/protocols/bookProtocols";
-import { createComment } from "../../../src/adapters/ormAdapter/protocols/commentProtocols";
+import { createComment, deleteComment, getAllComments, getCommentById, updateComment } from "../../../src/adapters/ormAdapter/protocols/commentProtocols";
 import { createUser } from "../../../src/adapters/ormAdapter/protocols/userProtocols";
-import { IBook } from "../../../src/entities/Book";
 import CleanDataBase from "../../util/CleanDataBase"
+import { Comment } from "../../../src/entities/Comment";
 
 interface ICommentToBeSearch {
     id?: string;
@@ -140,25 +140,62 @@ describe('Testes do CommentRepository', () => {
 
     it('Deve deletar um comentário', async () => {
        
+        await deleteComment.execute(idCommentToBeDelete)
+            .then(result => {
 
+                expect(result.message).toBe('Comentário deletado com sucesso!')
+                expect(result).not.toBeInstanceOf(Comment)
+
+            })
 
     })
 
     it('Deve buscar todos os comentários', async () => {
        
+        await getAllComments.execute(bookId)
+            .then(result => {
 
+                expect(result.length).toBeGreaterThan(0)
+                expect(result[0]).toBeInstanceOf(Comment)
+
+            })
 
     })
 
     it('Deve buscar um comentário por id', async () => {
        
+        await getCommentById.execute(idCommentToBeSearch)
+            .then(result => {
 
+                const equalValue: ICommentToBeSearch = {
+                    id: idCommentToBeSearch,
+                    comment: "Um comentário para busca",
+                    bookId: bookId,
+                    userId: userId
+                }
 
+                expect(result).toBeInstanceOf(Comment)
+                expect(result?.props).toEqual(equalValue)
+
+            })
     })
 
     it('Deve atulizar um comentário', async () => {
        
+        const updatedValues = {
+            id: idCommentToBeUpdate,
+            comment: "Um comentário já atualizado",
+            bookId: bookId,
+            userId: userId
+        }
 
+        await updateComment.execute(updatedValues)
+            .then(result => {
+
+                expect(result).toBeInstanceOf(Comment)
+                expect(result.props).toEqual(updatedValues)
+
+            })
 
     })
 
