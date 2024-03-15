e1d3e6c21c86f6d52b0e0b39c9ada52a62f57d5c
joão <joaopedrot1912@gmail.com>
2024-03-15 15:08:41 -0300

    errors

--- src/interface/controllers/utils/ServerResponse.ts
+++ src/interface/controllers/utils/ServerResponse.ts
@@ -1,4 +1,5 @@
 import { HttpResponse } from "../../../adapters/HTTPAdapter/protocol";
+import { CustomError } from "./CustomError";
 
 
 class ServerResponse {
@@ -10,6 +11,29 @@ class ServerResponse {
 
     ok(message: any): void {
         this.response.status(200).json(message);
+    }
+
+    static badRequest(name: string, message: string): CustomError {
+        return new CustomError(name, message, 400);
+    }
+    static notAuthorized(name: string, message: string): CustomError {
+        return new CustomError(name, message, 401);
+    }
+
+    static notFound(name: string,message: string): CustomError {
+        return new CustomError(name, message, 404);
+    }
+
+    static conflict(name: string, message: string): CustomError {
+        return new CustomError(name, message, 409);
+    }
+
+    static missingParameters(name: string, message: string): CustomError {
+        return new CustomError(name, message, 422);
+    }
+
+    static serviceUnavailable(name: string,message: string): CustomError {
+        return new CustomError(name, message, 503);
     }
 
 }
