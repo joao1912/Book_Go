b77f025c1f041027a82739c1f32e52e15a8276ef
joão <joaopedrot1912@gmail.com>
2024-03-18 08:54:40 -0300

    pronto

--- src/usecases/user/CreateUserUseCase.ts
+++ src/usecases/user/CreateUserUseCase.ts
@@ -12,7 +12,7 @@ export class CreateUserUseCase {
 
     async execute(userData: IUser) {
 
-        const data = validatorAdapter.validate<IUser, typeof userSchema.shape>(userData, userSchema)
+        const data = validatorAdapter.validate<IUser, typeof userSchema['_output']>(userData, userSchema)
 
         const userInstance = new User(data)
         const user = await this.userService.execute(userInstance)
