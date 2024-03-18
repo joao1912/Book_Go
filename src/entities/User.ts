b81d6969bb2a871106050803dd39a2df90c5cc15
João Pedro T <joaopedrot1912@gmail.com>
2024-03-18 16:17:37 -0300

    Update User.ts

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,13 +1,13 @@
-import { z } from "zod"
+import { ZodType, z } from "zod"
 
-/* export interface IUser  {
-    id?: string
-    username: string
-    password: string
-    email: string 
-    telephone: string
-    favoritesBooks?: number[]
-} */
+export interface IUser  {
+    id?: string;
+    username: string;
+    password: string;
+    email: string ;
+    telephone: string;
+    favoritesBooks?: number[];
+} 
 
 export const userSchema = z.object({
     id: z.string().optional(),
@@ -15,24 +15,22 @@ export const userSchema = z.object({
     password: z.string().min(6, { message: 'A Senha precisa de no minimo 6 caracteres.' }),
     email: z.string().email().max(40, { message: 'O email pode ter no maximo 40 caracteres.' }),
     telephone: z.string(),
-    favoritesBooks: z.array(z.number()).optional() 
-})
-
-export type IUser = z.infer<typeof userSchema>
+    favoritesBooks: z.array(z.number()).optional()
+}) satisfies ZodType<IUser>
 
 export class User {
-    
+
     readonly props: IUser
 
     constructor(props: IUser) {
-     
-      const  {id, username,  password, email, telephone, favoritesBooks = []} = props
+
+        const { id, username, password, email, telephone, favoritesBooks = [] } = props
         this.props = props
 
-       
+
     }
 
-   
+
     get User() {
         return {
             id: this.props.id,
