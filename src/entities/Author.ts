0cf24d57adc7a6af23cadba3ac54f9bc2f718beb
joão <joaopedrot1912@gmail.com>
2024-03-18 00:05:03 -0300

    zod na entidade

--- src/entities/Author.ts
+++ src/entities/Author.ts
@@ -1,8 +1,18 @@
-export interface IAuthor {
+import { z } from "zod";
+
+/* export interface IAuthor {
     id?: string;
     name: string;
     description: string;
-}
+} */
+
+const authorSchema = z.object({
+    id: z.string().optional(),
+    name: z.string().min(12, { message: 'O nome precisa de no minimo 12 caracteres.' }),
+    description: z.string().min(20, { message: 'A descrição precisa de no minimo 20 caracteres.' })
+})
+
+export type IAuthor = z.infer<typeof authorSchema>
 
 export class Author {
    
